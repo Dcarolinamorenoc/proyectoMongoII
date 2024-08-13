984c505a39b3ba33e4f651155b8d1842039c9aaf
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-08-13 13:04:46 -0500

    feat: :bug: Realizando arreglos

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const app = express();
-const peliculaRoutes = require("./router/pelicula.routes");
+const peliculaRoutes = require("./server/router/pelicula.routes");
 require('dotenv').config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", function (req, res) {
+    res.sendFile(`${process.env.EXPRESS_STATIC}/index.html`, { root: __dirname });
+});
 app.use('/api', peliculaRoutes);
 
-/*app.get("/", function (req, res) {
-    res.sendFile(`${process.env.EXPRESS_STATIC}/index.html`, { root: __dirname });
-});*/
 
 app.listen({
     host: process.env.EXPRESS_HOST,
