a187acd90f3195f27a7d7927f28ba24aa37c334b
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-08-13 12:47:54 -0500

    feat: :bug: Se soluciono el bug que se estaba presentando con la Api de peliculas
    La api ya funciona correctamente

--- server/app.js
+++ server/app.js
@@ -1,42 +1,20 @@
-require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const peliculaRoutes = require('./router/pelicula.routes');
-const boletoRoutes = require('./router/boleto.routes');
-const reservaRoutes = require('./router/reserva.routes');
-const tarjetaVipRoutes = require('./router/tarjetaVip.routes');
-const usuarioRoutes = require('./router/usuario.routes');
-
 const app = express();
+const peliculaRoutes = require("./router/pelicula.routes");
+require('dotenv').config();
 
-app.use(express.static(process.env.EXPRESS_STATIC));
-
-// Habilitar CORS para todas las rutas
-app.use(cors());
-
-// Middleware para parsear JSON
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Servir archivos estáticos desde la carpeta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', peliculaRoutes);
 
-// Rutas API
-app.use('/api/pelicula', peliculaRoutes);
-app.use('/api/boleto', boletoRoutes);
-app.use('/api/reserva', reservaRoutes);
-app.use('/api/tarjeta-vip', tarjetaVipRoutes);
-app.use('/api/usuario', usuarioRoutes);
-
-app.get("/", function (req, res) {
+/*app.get("/", function (req, res) {
     res.sendFile(`${process.env.EXPRESS_STATIC}/index.html`, { root: __dirname });
-});
-
+});*/
 
 app.listen({
-    host: process.env.EXPRESS_HOST, 
+    host: process.env.EXPRESS_HOST,
     port: parseInt(process.env.EXPRESS_PORT)
 }, () => {
-    
     console.log(`Servidor corriendo en: http://${process.env.EXPRESS_HOST}:${process.env.EXPRESS_PORT}`);
 });
